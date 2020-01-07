344823c830e92304a3c458d6c70cda01f660cf2c
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-07 21:07:33 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,6 @@
 class Owner
   
   @@all = []
-  @@pets = {:cats => [], :dogs => []}
   
   attr_accessor :owner
   attr_reader :name, :species 
@@ -46,8 +45,7 @@ end
 
 
 def buy_cat(cat)
-# Cat.new(cat, self)
-@@pets[:cats] << Cat.new(cat)
+Cat.new(cat, self)
 end 
 
 def buy_dog(dog)
