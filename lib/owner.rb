7bd6bcc19ae1e47e84b909050dc6ae8011f05905
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-07 21:31:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,7 +63,7 @@ end
 
 
 def sell_pets 
-if Cat.all.each {|cat| cat.mood = "nervous"} && Dog.all.each {|dog| dog.mood = "nervous"}
+  Cat.all.each {|cat| cat.mood = "nervous"} && Dog.all.each {|dog| dog.mood = "nervous"}
 
 end 
   
