20132bb8cc6cecd5fafd9f4067e149e7dfbac559
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-07 21:40:48 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -68,7 +68,7 @@ def sell_pets
 end 
 
 def list_pets 
-  return "I have #{Dog.length} dog(s), and  #{Cat.length} cat(s)." 
+  return "I have #{dogs.length} dog(s), and  #{cats.length} cat(s)." 
 end 
   
 end
